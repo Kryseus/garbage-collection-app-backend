4258bb9fa8d91d533c7c5dd356e1840592eea199
Philip Tronser <tronser@web.de>
2022-02-19 17:02:26 +0100

    Controller modified

--- Controllers/garbageController.js
+++ Controllers/garbageController.js
@@ -5,6 +5,14 @@ const garbageData = require("../db/Garbage.json");
 
 export const getAll = (req, res) => res.status(200).json(garbageData);
 
+export const getListOfStreets = (req, res) => {
+  const streets = garbageData.map((entry) => ({
+    id: entry.id,
+    street: entry.street,
+  }));
+  res.status(200).json(streets);
+};
+
 export const getSingle = (req, res) => {
   const { id } = req.params;
   const garbage = garbageData.find((garbage) => garbage.id === Number(id));
--- router/garbageRouter.js
+++ router/garbageRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 //import express from 'express';
-import { getAll, getSingle } from "../Controllers/garbageController.js";
+import { getAll, getListOfStreets, getSingle } from "../Controllers/garbageController.js";
 
 const garbageRouter = Router();
 //const garbageRouter = express.Router();
 
 garbageRouter.get("/", getAll);
+garbageRouter.get("/streets", getListOfStreets);
 garbageRouter.get("/:id", getSingle);
 
 export default garbageRouter;
