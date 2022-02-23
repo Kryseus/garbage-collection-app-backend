f85648c9b4cc1b2367d3b8d6be1346ba826e0061
Philip Tronser <tronser@web.de>
2022-02-23 13:43:19 +0100

    date format fixed

--- router/garbageRouter.js
+++ router/garbageRouter.js
@@ -1,9 +1,7 @@
 import { Router } from "express";
-//import express from 'express';
 import { getAll, getListOfStreets, getSingle } from "../Controllers/garbageController.js";
 
 const garbageRouter = Router();
-//const garbageRouter = express.Router();
 
 garbageRouter.get("/", getAll);
 garbageRouter.get("/streets", getListOfStreets);
